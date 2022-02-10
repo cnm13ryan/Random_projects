b43f59055c013c16eb6909e0cc505326290490b9
cnm13ryan <70359515+cnm13ryan@users.noreply.github.com>
2022-02-10 16:58:00 +0000

    Update outlet_heat_exchanger.py

--- outlet_heat_exchanger.py
+++ outlet_heat_exchanger.py
@@ -7,15 +7,8 @@
 # For this purpose, a class "Chem_Species" is created to store the data and functions to compute the partial vapour pressures and the individual composition of species in binary phases.
 # 
 
-# In[1]:
-
-
 import numpy as np
 import matplotlib.pyplot as plt
-
-
-
-# In[2]:
 
 
 class Chem_Species(object):
@@ -24,48 +17,16 @@ class Chem_Species(object):
         self.name = name;
         self.P_satur = P_satur;
     
-    # Compute the saturation pressure given the known temperature of 20 degrees Celsius
+   
     def P_satur(self, T):
+        """Compute the saturation pressure given the known temperature """
         raise Exception('P_satur() has not been defined for ' + self.name);
+
         
-    
-# Antoine coefficient referenced from Yaw's handbook 
-act_Pp =  Chem_Species('Acetone',  lambda T: 10**(7.31414 - 1315.67/(T + 240.479)));
-w_Pp   =  Chem_Species('Water',  lambda T: 10**(8.05573 - 1723.64/(T + 233.076)));
-ipa_Pp =  Chem_Species('IPA',  lambda T: 10**(7.83056 - 1483.3/(T + 217.413)));
 
-
-# In[3]:
-
-
-T_sys = 20; # degree Celsius
-P_sys = 1200; # mm Hg
-
-
-S4_act_Pp = act_Pp.P_satur(T_sys); # Expected: 183.3057966039555
-# name1 = act_Pp.name
-print(S4_act_Pp);
-
-S4_w_Pp = w_Pp.P_satur(T_sys); # Expected: 17.578005499499085
-print(S4_w_Pp);
-
-S4_ipa_Pp = ipa_Pp.P_satur(T_sys); # Expected: 38.2646426454334
-print(S4_ipa_Pp);
-
-
-# In[4]:
-
-
-# Create a list of Vapour Pressure 
-vp_S4 = [S4_act_Pp, S4_w_Pp, S4_ipa_Pp];
-print(vp_S4)
-
-
-# In[5]:
-
-
-# Function to calculate the pressure ratio
 def pressure_ratio(list_Pp, P_sys=1200):
+    """Function to calculate the pressure ratio"""
+    
     Pr_i = [];
     i = 0;
     while i < len(list_Pp):
@@ -74,21 +35,8 @@ def pressure_ratio(list_Pp, P_sys=1200):
         i += 1;
     return Pr_i;
 
-
-# In[6]:
-
-
-# Create a list of Composition of Species
-composition_S4_i = [0.376, 0.206, 0.0417];
-
-Pp_i = pressure_ratio(vp_S4)
-print(Pp_i);
-
-
-# In[7]:
-
-
 def calcuate_composition(list_com, list_Pr):
+    """ Function to calculate the composition of the species in their respective phases"""
     
     X_i = [];
     Y_i = [];
@@ -102,37 +50,52 @@ def calcuate_composition(list_com, list_Pr):
     return X_i, Y_i;
 
 
-# In[16]:
+
+# Antoine coefficient referenced from Yaw's handbook 
+act_Pp =  Chem_Species('Acetone',  lambda T: 10**(7.31414 - 1315.67/(T + 240.479)));
+w_Pp   =  Chem_Species('Water',  lambda T: 10**(8.05573 - 1723.64/(T + 233.076)));
+ipa_Pp =  Chem_Species('IPA',  lambda T: 10**(7.83056 - 1483.3/(T + 217.413)));
 
 
+# Partial Pressure of the three components (Acetone, Water, IPA)
+S4_act_Pp = act_Pp.P_satur(T_sys); # Expected: 183.3057966039555
+S4_w_Pp = w_Pp.P_satur(T_sys); # Expected: 17.578005499499085
+S4_ipa_Pp = ipa_Pp.P_satur(T_sys); # Expected: 38.2646426454334
+
+
+# Temperature and Pressure of the system
+T_sys = 20; # degree Celsius
+P_sys = 1200; # mm Hg
+
+
+# Create a list of Vapour Pressure 
+vp_S4 = [S4_act_Pp, S4_w_Pp, S4_ipa_Pp];
+# print(vp_S4)
+
+
+# Create a list of Composition of Species
+composition_S4_i = [0.376, 0.206, 0.0417];
+
+
+# Create a list of Pressure ratios
+Pp_i = pressure_ratio(vp_S4)
+# print(Pp_i);
+
+
+# Calculate the composition of the different species
 X_S4, Y_S4 = calcuate_composition(composition_S4_i, Pp_i  )
-
-
-# In[17]:
-
-
 print(f" X_S4 \tX_act \tX_w \tX_ipa\n\t{X_S4} \n\n Y_S4 \tY_act \tY_w \tY_ipa\n\t{Y_S4} ")
 
-
-# In[18]:
-
-
 hydrogen_composition = 1 - composition_S4_i[0] - composition_S4_i[1] - composition_S4_i[2]
-
-
-# In[19]:
-
-
 print(f"\nY_S4_Hydrogen is: {hydrogen_composition}");
 
 
-# In[20]:
-
 # For purposes of checking
 hydrogen_composition1 = 1 - X_S4[0] - X_S4[1] - X_S4[2] - Y_S4[0] - Y_S4[1] - Y_S4[2]
+
 check = hydrogen_composition - hydrogen_composition1;
-if check != 0:
-    print("Error in Calculation");
+
+if check != 0: print("Error in Calculation");
 
 
 
